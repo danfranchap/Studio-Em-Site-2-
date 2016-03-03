21051b4b60a64c15771b7be6b7fb0939daa7fd6c
danfranchap <DanFranChap@Danielles-MacBook-Pro.local>
2016-03-02 20:42:33 -0500

    First Commit - Added Everything

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    
+    console.log( "Ready!" );
+
+    $('.grid').masonry({
+	  itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+	  columnWidth: '.grid-sizer',
+	  percentPosition: true
+	});
+}); // document.ready
