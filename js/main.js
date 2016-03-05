e05f85fcad15c9e562a5f742ea6e6e25a0ddaa14
danfranchap <DanFranChap@Danielles-MacBook-Pro.local>
2016-03-05 15:13:15 -0500

    Top Nav Responsive fixed!
    New CSS media query to add a breakpoint at 480px.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,56 @@
+//var $window = $(window);
+
 $( document ).ready(function() {
     
     console.log( "Ready!" );
 
-    $('.grid').masonry({
+      
+
+
+   
+}); // document.ready
+
+
+
+/* TRASH!!
+
+
+ $('.grid').masonry({
 	  itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
 	  columnWidth: '.grid-sizer',
 	  percentPosition: true
 	});
-}); // document.ready
+
+
+
+function checkWidth() {
+
+	    if ($window.width() =< 480) {
+
+	        if ($("#navLogoBlock").hasClass("col-xs-4")){
+	        	$("#navLogoBlock").removeClass("col-xs-4").addClass("col-xs-12");
+	        } else if ( $("#navLogoBlock").hasClass("col-xs-12")){
+
+	        } else {
+	        	$("#navLogoBlock").addClass("col-xs-12");
+	        }
+	    }
+	    
+
+	    if ($window.width() > 480) {
+
+	    	if ($("#navLogoBlock").hasClass("col-xs-12")){
+	        	$("#navLogoBlock").removeClass("col-xs-12").addClass("col-xs-4");
+	        } else if ( $("#navLogoBlock").hasClass("col-xs-4")){
+
+	        } else {
+	        	$("#navLogoBlock").addClass("col-xs-4");
+	        }
+
+
+	    }
+
+    }
+
+
+*/
